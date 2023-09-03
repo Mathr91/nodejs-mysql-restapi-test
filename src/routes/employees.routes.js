f971a9c057528c492afabbbd96642b80553e415b
Mathr91 <matiashrdev@gmai.com>
2023-09-03 20:40:33 -0300

    fix endpoint employees/:id

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -4,9 +4,9 @@ import { getEmployees, getEmployee, createEmployees, updateEmployees, deleteEmpl
 const router = Router();
 
 router.get('/employees', getEmployees);
-router.get('/employee/:id', getEmployee);
+router.get('/employees/:id', getEmployee);
 router.post('/employees', createEmployees);
 router.put('/employees/:id', updateEmployees);
 router.delete('/employees/:id', deleteEmployees);
 
-export default router;
+export default router;n
